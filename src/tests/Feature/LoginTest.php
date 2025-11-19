0e87fb09f6ab09b149e2f98d87a25a144295700d
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-11-19 23:38:48 +0900

    ログインテスト

---
+++ src/tests/Feature/LoginTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    /**
+     * @test
+     * メールアドレスが未入力の場合、バリデーションメッセージが表示される（一般ユーザー）
+     * 1. ユーザーを登録する(seedで登録済み)
+     * 2. メールアドレス以外のユーザー情報を入力する
+     * 3. ログインの処理を行う
+     */
+    public function login_userEmail_validationMessage()
+    {
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['email' => 'メールアドレスを入力してください']);
+    }
+
+    /**
+     * @test
+     * パスワードが未入力の場合、バリデーションメッセージが表示される（一般ユーザー）
+     * 1. ユーザーを登録する
+     * 2. パスワード以外のユーザー情報を入力する
+     * 3. ログインの処理を行う
+     */
+    public function login_userPassword_validationMessage()
+    {
+        $response = $this->post('/login', [
+            'email' => 'general1@example.com',
+            'password' => '',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['password' => 'パスワードを入力してください']);
+    }
+
+    /**
+     * @test
+     * 登録内容と一致しない場合、バリデーションメッセージが表示される（一般ユーザー）
+     * 1. ユーザーを登録する
+     * 2. 誤ったメールアドレスのユーザー情報を入力する
+     * 3. ログインの処理を行う
+     */
+    public function login_user_validationMessage()
+    {
+        $response = $this->post('/login', [
+            'email' => 'general1@example.com',
+            'password' => 'wrongpassword',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['email' => 'ログイン情報が登録されていません']);
+    }
+
+    /**
+     * @test
+     * メールアドレスが未入力の場合、バリデーションメッセージが表示される（管理者）
+     * 1. ユーザーを登録する（seedで登録済み）
+     * 2. メールアドレス以外のユーザー情報を入力する
+     * 3. ログインの処理を行う
+     */
+    public function login_adminEmail_validationMessage()
+    {
+        $response = $this->post('/login', [
+            'email' => '',
+            'password' => Hash::make(env('ADMIN_PASSWORD', 'adminpassword')),
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['email' => 'メールアドレスを入力してください']);
+    }
+
+    /**
+     * @test
+     * パスワードが未入力の場合、バリデーションメッセージが表示される（管理者）
+     * 1. ユーザーを登録する
+     * 2. パスワード以外のユーザー情報を入力する
+     * 3. ログインの処理を行う
+     */
+    public function login_adminPassword_validationMessage()
+    {
+        $response = $this->post('/login', [
+            'email' => env('ADMIN_EMAIL', 'admin@example.com'),
+            'password' => '',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['password' => 'パスワードを入力してください']);
+    }
+
+    /**
+     * @test
+     * 登録内容と一致しない場合、バリデーションメッセージが表示される（管理者）
+     * 1. ユーザーを登録する
+     * 2. 誤ったメールアドレスのユーザー情報を入力する
+     * 3. ログインの処理を行う
+     */
+    public function login_admin_validationMessage()
+    {
+        $response = $this->post('/login', [
+            'email' => env('ADMIN_EMAIL', 'admin@example.com'),
+            'password' => 'wrongpassword',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors(['email' => 'ログイン情報が登録されていません']);
+    }
+}
